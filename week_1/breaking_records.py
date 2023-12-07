da53f21063d453e4e849608a4e03fbcc1c682fc6
Jickx <valentin.sharonov@gmail.com>
2023-12-07 17:12:40 +0300

    add breaking_records.py

---
+++ week_1/breaking_records.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+def breaking_records(scores):
+    result = [0, 0]
+    maxim = minim = scores[0]
+    for i in scores:
+        if i > maxim:
+            result[0] += 1
+            maxim = i
+        elif i < minim:
+            result[1] += 1
+            minim = i
+    return result
+
+
+class TestBreakingRecords(TestCase):
+    def test_breaking_records(self):
+        self.assertEqual(breaking_records([10, 5, 20, 20, 4, 5, 2, 25, 1]), [2, 4])
+        self.assertEqual(breaking_records([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]), [4, 0])
