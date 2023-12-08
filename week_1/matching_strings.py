1768066640f2917c5725b5d65824713c84873025
Jickx <valentin.sharonov@gmail.com>
2023-12-08 19:31:19 +0300

    add matching_strings.py O(n)

--- week_1/matching_strings.py
+++ week_1/matching_strings.py
@@ -1,16 +1,19 @@
 def matching_strings():
     result = []
+    strings_dict = {i: 0 for i in set(strings)}
+    for i in strings:
+        strings_dict[i] += 1
     for i in queries:
-        ctr = 0
-        for j in strings:
-            if i == j:
-                ctr += 1
-        result.append(ctr)
+        if i in strings_dict:
+            result.append(strings_dict[i])
+        else:
+            result.append(0)
     return result
 
 
-strings= ['aba','baba','aba','xzxb']
-queries = ['aba', 'xzxb', 'ab']
-expected = [2, 1, 0]
+strings = ['abcde', 'sdaklfj', 'asdjf', 'na', 'basdn', 'sdaklfj', 'asdjf', 'na', 'asdjf', 'na', 'basdn', 'sdaklfj',
+           'asdjf']
+queries = ['abcde', 'sdaklfj', 'asdjf', 'na', 'basdn']
+expected = [1, 3, 4, 3, 2]
 
 print(matching_strings())
